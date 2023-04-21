0ec608ac13d9f859fb72bbfa01c328a1da3a770e
Simon <simimueller@hotmail.com>
2023-04-21 14:05:21 +0200

    add script

--- w4-js/Class-List-Project/script.js
+++ w4-js/Class-List-Project/script.js
@@ -52,112 +52,64 @@ const zurich = document.querySelector("#zurich");
 const bern = document.querySelector("#bern");
 const lausanne = document.querySelector("#lausanne");
 
+const generateHTML = (item) => {
+    const personArticle = document.createElement("div");
+    personArticle.setAttribute("class", "article");
+
+    const personImage = document.createElement("img");
+    personImage.src = item.img;
+    personArticle.appendChild(personImage);
+
+    const overlay = document.createElement("div");
+    overlay.setAttribute("class", "overlay");
+    personArticle.appendChild(overlay);
+
+    const textDiv = document.createElement("div");
+    textDiv.setAttribute("class", "text");
+
+    const personName = document.createElement("h2");
+    personName.innerHTML = item.name;
+    textDiv.appendChild(personName);
+
+    const personLocation = document.createElement("p");
+    personLocation.innerHTML = item.location;
+    textDiv.appendChild(personLocation);
+
+    personArticle.appendChild(textDiv);
+    return personArticle;
+};
+
 zurich.addEventListener("click", () => {
     removeAllChildNodes(main);
     zurich_list.forEach((item) => {
-        const personArticle = document.createElement("div");
-        personArticle.setAttribute("class", "article");
-
-        const personImage = document.createElement("img");
-        personImage.src = item.img;
-        personArticle.appendChild(personImage);
-
-        const personName = document.createElement("h2");
-        personName.innerHTML = item.name;
-        personArticle.appendChild(personName);
-
-        const personLocation = document.createElement("p");
-        personLocation.innerHTML = item.location;
-        personArticle.appendChild(personLocation);
-
-        main.appendChild(personArticle);
+        main.appendChild(generateHTML(item));
     });
 });
 
 bern.addEventListener("click", () => {
     removeAllChildNodes(main);
     bern_list.forEach((item) => {
-        const personArticle = document.createElement("div");
-        personArticle.setAttribute("class", "article");
-
-        const personImage = document.createElement("img");
-        personImage.src = item.img;
-        personArticle.appendChild(personImage);
-
-        const personName = document.createElement("h2");
-        personName.innerHTML = item.name;
-        personArticle.appendChild(personName);
-
-        const personLocation = document.createElement("p");
-        personLocation.innerHTML = item.location;
-        personArticle.appendChild(personLocation);
-
-        main.appendChild(personArticle);
+        main.appendChild(generateHTML(item));
     });
 });
 
 lausanne.addEventListener("click", () => {
     removeAllChildNodes(main);
     lausanne_list.forEach((item) => {
-        const personArticle = document.createElement("div");
-        personArticle.setAttribute("class", "article");
-
-        const personImage = document.createElement("img");
-        personImage.src = item.img;
-        personArticle.appendChild(personImage);
-
-        const personName = document.createElement("h2");
-        personName.innerHTML = item.name;
-        personArticle.appendChild(personName);
-
-        const personLocation = document.createElement("p");
-        personLocation.innerHTML = item.location;
-        personArticle.appendChild(personLocation);
-
-        main.appendChild(personArticle);
+        main.appendChild(generateHTML(item));
     });
 });
 
 all.addEventListener("click", () => {
     removeAllChildNodes(main);
     powercoders.forEach((item) => {
-        const personArticle = document.createElement("div");
-        personArticle.setAttribute("class", "article");
-
-        const personImage = document.createElement("img");
-        personImage.src = item.img;
-        personArticle.appendChild(personImage);
-
-        const personName = document.createElement("h2");
-        personName.innerHTML = item.name;
-        personArticle.appendChild(personName);
-
-        const personLocation = document.createElement("p");
-        personLocation.innerHTML = item.location;
-        personArticle.appendChild(personLocation);
-
-        main.appendChild(personArticle);
+        main.appendChild(generateHTML(item));
     });
 });
 
 window.addEventListener("load", (event) => {
     powercoders.forEach((item) => {
-        const personArticle = document.createElement("div");
-        personArticle.setAttribute("class", "article");
-
-        const personImage = document.createElement("img");
-        personImage.src = item.img;
-        personArticle.appendChild(personImage);
-
-        const personName = document.createElement("h2");
-        personName.innerHTML = item.name;
-        personArticle.appendChild(personName);
-
-        const personLocation = document.createElement("p");
-        personLocation.innerHTML = item.location;
-        personArticle.appendChild(personLocation);
-
-        main.appendChild(personArticle);
+        main.appendChild(generateHTML(item));
     });
     powercoders.forEach((item) => {
         if (item.location == "Zurich") {
